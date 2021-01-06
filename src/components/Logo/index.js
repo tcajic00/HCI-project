0286c163b428001a0197574a0c23d51379c8a67e
Musa17 <amusul00@fesb.hr>
2021-01-06 20:41:04 +0100

    Adding logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const Logo = () => (
+  <section className={styles.logo}>
+    <span className={styles.logoText}>G4U</span>
+  </section>
+)
+
+export default Logo
