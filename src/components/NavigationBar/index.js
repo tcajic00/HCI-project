48766d384acf9576412e59f07fa3745569a6024f
Musa17 <amusul00@fesb.hr>
2021-01-06 21:42:39 +0100

    Adding NavigationBar part 1

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 import styles from './style.module.css'
 
-const NavigationBar = () => <section></section>
+const NavigationBar = () => (
+  <nav className={styles.navigationBar}>
+      <li className={styles.active}>Home</li>
+      <li>About</li>
+      <li>Games</li>
+      <li>News & Reviews</li>
+      <li>Contact</li>
+      <li>Cart</li>
+      <li>Sign In</li>
+  </nav>
+)
 
 export default NavigationBar
