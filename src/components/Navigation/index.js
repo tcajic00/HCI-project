004d15e9b036b1b1e47f22f6e74cb9107bc0746f
Musa17 <amusul00@fesb.hr>
2021-01-06 20:49:54 +0100

    Adding Navigation

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import NavigationBar from '../NavigationBar'
+import styles from './style.module.css'
+
+const Navigation = () => (
+  <section>
+    <NavigationBar />
+  </section>
+)
+
+export default Navigation
