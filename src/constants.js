abbf0d9e03d1ed3535978abbab013bd45437c3ea
Musa17 <amusul00@fesb.hr>
2021-01-18 23:06:17 +0100

    Adding news page

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const navs = [
     {tab: 'Home', to: '/'},
     {tab: 'Games', to: '/'},
-    {tab: 'News & Reviews', to: '/'},
+    {tab: 'News & Reviews', to: '/news_reviews'},
     {tab: 'Contact', to: '/contact'},
     {tab: 'About', to: '/about'}, 
     {tab: 'Cart', to: '/'},
---
+++ src/pages/news_reviews.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import NavigationHeader from '../components/NavigationHeader'
+import Footer from '../components/Footer'
+
+const NewsPage = () => (
+  <main>
+    <NavigationHeader activeTab = "News & Reviews" />
+    <Footer />
+  </main>
+)
+
+export default NewsPage
