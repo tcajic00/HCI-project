2351b6493ed8cd25f03910672e6c6fa9c7588273
Tomislav Cajic <tcajic00@fesb.hr>
2021-02-08 15:04:16 +0100

    Enabled login feature with hardcoded inputs

--- src/components/LogInHeader/index.js
+++ src/components/LogInHeader/index.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import { Button, TextField } from '@material-ui/core'
 
-import Logo from '../Logo'
+import styles from './style.module.css'
+
+
 
 const LogInHeader = () => (
-    <header>
-        <Logo />
-    </header>
+    <main className={styles.body}>
+        <TextField 
+            id="" 
+            margin="normal"
+            required
+            label="username" 
+            variant="outlined" 
+        />
+        <TextField 
+            id="password" 
+            label="password" 
+            variant="outlined" 
+            type="password"
+            name="password"
+            required
+            margin="normal"
+        />
+        <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        >
+            Log In
+        </Button>
+    </main>
 )
 
 export default LogInHeader;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,77 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {navigate} from 'gatsby'
 
-import LogInHeader from '../components/LogInHeader'
+import styles from './login.module.css'
+import { Button, TextField } from '@material-ui/core'
+import Logo from '../components/Logo'
 
-const LogIn = () => (
-    <main>
-        <LogInHeader />
-    </main>
-)
+const users = [
+{
+  username: "amusul00",
+  password: "12345"
+},
+{
+  username: "tcajic00",
+  password: "12345"
+},
+]
 
-export default LogIn;
+const LogIn = () => {
+  const [username, setUserName] = useState()
+  const [password, setPassword] = useState()
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
 
+  const submit = () => {
+    setError(false)
+    setLoading(true)
+    setTimeout(() => {
+      const loginSuccessful = !!users.find(user => user.username === username && user.password === password)
+      setLoading(false)
+      if (loginSuccessful) {
+        localStorage.setItem("loggedIn", username)
+        setError('Success')
+        return navigate('/')
+      }
+        setError('Wrong username or password')
+    }, 1500)
+  }
+  
+  return (
+  <main className={styles.background} onKeyDown={key => {
+    if (key.key === "Enter")
+      return submit()
+  }}>
+    <section className={styles.container}>
+        <Logo />
+      <TextField 
+        margin="normal"
+        variant="outlined"
+        name="username"
+        label="Username"
+        required
+        onChange={e => setUserName(e.target.value)}
+      />
+      <TextField 
+        margin="normal"
+        variant="outlined"
+        name="password"
+        label="Password"
+        type="password"
+        required
+        onChange={e=> setPassword(e.target.value)}
+      />
+      <p className={`${styles[error !== "Success" ? 'error' : 'success']} ${error ? styles.show : ''}`}>{error}</p>
+      <Button 
+      className={styles.loginButton} 
+      onClick={() => submit()}
+      variant="contained"
+      color="primary"
+      >
+        {loading ? 'Loading...' : 'Login'}
+      </Button>
+    </section>
+  </main>
+)}
+
+export default LogIn
