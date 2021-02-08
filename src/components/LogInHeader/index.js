6f894d2543c4d9adb8cbcd4bacc93a0a74b16870
Tomislav Cajic <tcajic00@fesb.hr>
2021-02-08 14:05:09 +0100

    creating LogIn page and header component for that page

---
+++ src/components/LogInHeader/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Logo from '../Logo'
+
+const LogInHeader = () => (
+    <header>
+        <Logo />
+    </header>
+)
+
+export default LogInHeader;
---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import LogInHeader from '../components/LogInHeader'
+
+const LogIn = () => (
+    <main>
+        <LogInHeader />
+    </main>
+)
+
+export default LogIn;
+
