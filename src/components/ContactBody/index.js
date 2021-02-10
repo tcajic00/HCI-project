76331083632978146403b4b1d3f37351fc61f36a
Tomislav Cajic <tcajic00@fesb.hr>
2021-02-10 20:43:52 +0100

    fixing css on contact page

--- src/components/ContactBody/index.js
+++ src/components/ContactBody/index.js
@@ -8,40 +8,46 @@ import styles from './style.module.css'
 const ContactBody = () => (
     <section className={styles.section}>
         <div className={styles.cardsContainer}>
-
-            <p className={styles.cardTitle}>E-mail:</p>
-            <div className={styles.card}>
-                <div className={styles.text}>
-                    <p className={styles.firstText}>info@g4u.hr</p>
-                </div>
-                <div className={styles.image}>
-                    <MailPhoto />
-                </div>
-            </div>
-
-            <p className={styles.cardTitleReverse}>Phone:</p>
-            <div className={styles.cardReverse}>
-                <div className={styles.imageReverse}>
-                    <PhoneImage />
-                </div>
-                <div className={styles.textReverse}>
-                    <p className={styles.firstTextReverse}>+385 12 345 6789</p>
+            <div className={styles.cont}>
+                <p className={styles.cardTitle}>E-mail:</p>
+                <div className={styles.card}>
+                    <div className={styles.text}>
+                        <p className={styles.firstText}>info@g4u.hr</p>
+                    </div>
+                    <div className={styles.image}>
+                        <MailPhoto />
+                    </div>
                 </div>
             </div>
-
-            <p className={styles.cardTitle}>Address:</p>
-            <div className={styles.cardLocation}>
-                <div className={styles.text}>
-                    <p className={styles.firstTextLocation}>Head Office:</p>
-                    <p className={styles.secondTextLocation}>Ulica Matice hrvatske 15, Split, Croatia</p>
-
-                    <p className={styles.firstTextLocation}>Research & Development:</p>
-                    <p className={styles.secondTextLocation}>Metković, Croatia</p>
-                </div>
-                <div className={styles.imageLocation}>
-                    <LocationImage />
+                
+            <div className={styles.contrev}>
+                <p className={styles.cardTitleReverse}>Phone:</p>
+                <div className={styles.cardReverse}>
+                    <div className={styles.imageReverse}>
+                        <PhoneImage />
+                    </div>
+                    <div className={styles.textReverse}>
+                        <p className={styles.firstTextReverse}>+385 12 345 6789</p>
+                    </div>
                 </div>
             </div>
+            
+            <div className={styles.contloc}>
+                <p className={styles.cardTitle}>Address:</p>
+                <div className={styles.cardLocation}>
+                    <div className={styles.text}>
+                        <p className={styles.firstTextLocation}>Head Office:</p>
+                        <p className={styles.secondTextLocation}>Ulica Matice hrvatske 15, Split, Croatia</p>
+
+                        <p className={styles.firstTextLocation}>Research & Development:</p>
+                        <p className={styles.secondTextLocation}>Metković, Croatia</p>
+                    </div>
+                    <div className={styles.imageLocation}>
+                        <LocationImage />
+                    </div>
+                </div>
+            </div>
+            
 
         </div>
     </section>
