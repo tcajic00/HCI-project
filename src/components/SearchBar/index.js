1286567a917062a244fa6d76644fb62a8a666564
Musa17 <amusul00@fesb.hr>
2021-02-12 17:05:31 +0100

    Changing background of searchBar in NavigationHeader

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -4,8 +4,8 @@ import { TextField } from '@material-ui/core'
 
 import styles from './style.module.css'
 
-const SearchBar = () => <form className={styles} noValidate autoComplete="off">
-<TextField id="outlined-basic" label="Search ..." variant="outlined" size="small"/>
+const SearchBar = () => <form className={styles.searchBar} noValidate autoComplete="off">
+<TextField id="outlined-basic" label="Search" variant="outlined" size="small"/>
 </form>
 
 export default SearchBar
