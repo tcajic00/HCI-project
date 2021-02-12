575cd9664fa9a06ffb7b20ea080ddd6b60c1b85b
Musa17 <amusul00@fesb.hr>
2021-02-12 16:32:12 +0100

    Making clikable logo

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Logo from '../Logo'
-
+import { Link } from 'gatsby'
 import SearchBar from '../SearchBar'
 import styles from './style.module.css'
 
 const Navigation = () => (
   <section className={styles.navigation}>
+    <Link to='/' style={{textDecoration: 'none'}}>
       <Logo />
-      <SearchBar />
+    </Link>
+    <SearchBar />
   </section>
 )
 
