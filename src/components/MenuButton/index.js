ab6d0de00b2794ba5e7abf44e6ac6a40e38dc01f
Musa17 <amusul00@fesb.hr>
2021-02-20 00:18:53 +0100

    Adding responsive CSS for Header section

---
+++ src/components/MenuButton/index.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import styles from './style.module.css'
+import { TextField } from '@material-ui/core'
+import { navs as navTabs } from '../../constants'
+import { Link } from 'gatsby'
+import ButtonStripes from '../../images/MenuStripes.png'
+import ButtonEscape from '../../images/MenuEscape.png'
+import Logo from '../../components/Logo'
+
+const loggedIn = () => !!localStorage.getItem('loggedIn');
+
+const useStyles = makeStyles({
+  list: {
+    width: 300,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+const MenuButton = ({ activeTab }) => {
+
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <div className={styles.card}>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+
+        <TextField 
+          className={styles.searchBar}
+          margin="dense"
+          variant="outlined"
+          name="Search"
+          label="Search"
+        />
+
+          <nav className={styles.navigationBar}>
+              {navTabs.map(({tab, to}) => (
+                <Link to={to} style={{textDecoration: 'none'}}>
+                    <li className={tab === activeTab ? styles.active : ''}>{tab}</li>
+                </Link>)
+              )}
+              <Link to={loggedIn() ? '/' : '/login'} >
+                <li onClick={loggedIn() ? () => localStorage.removeItem('loggedIn') : () => {}} className={"Log in" === activeTab ? styles.active : ''}>
+                  {loggedIn() ? 'Log out' : 'Log in'}
+                </li>
+              </Link>
+          </nav>
+      </div>
+    </div>
+  );
+
+  return (
+    <section className={styles.section}>
+      {['left'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <div className={styles.button} onClick={toggleDrawer(anchor, true)}>
+            <img src={state.left ? ButtonEscape : ButtonStripes} width='45px' height='45px'></img>
+          </div>
+          
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </section>
+  )
+}
+
+export default MenuButton
--- src/components/NavigationHeader/index.js
+++ src/components/NavigationHeader/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import Navigation from '../Navigation'
 import NavigationBar from '../NavigationBar'
-
+import MenuButton from '../MenuButton'
 import styles from './style.module.css'
 
 const NavigationHeader = ({ activeTab }) => (
   <section className={styles.navigationHeader}>
       <Navigation />
       <NavigationBar activeTab={activeTab}/>
+      <MenuButton activeTab={activeTab}/>
   </section>
 )
 
